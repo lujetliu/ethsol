41b270532525442ac9545a9cb0547f88d92e8bf6
lujetliu <462546997@qq.com>
2023-01-02 20:27:20 +0800

    func return

--- base/func.sol
+++ base/func.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-
 /*
 	函数类型
 	function <function name>(<parameter types>) {internal|external|public|private} [pure|view|payable] [returns (<return types>)]
@@ -74,6 +73,30 @@ contract FunctionTypes{
         minus();
         balance = address(this).balance;
     }
+
+
+	// 返回多个变量
+    function returnMultiple() public pure returns(uint256, bool, uint256[3] memory){
+		// TODO: memory
+        return(1, true, [uint256(1),2,5]);
+    }
+
+	// 命名式返回
+    function returnNamed() public pure returns(uint256 _number, bool _bool, uint256[3] memory _array){
+        _number = 2;
+        _bool = false;
+        _array = [uint256(3),2,1];
+    }
+
+	// 解构式赋值
+	// solidity使用解构式赋值的规则, 支持读取函数的全部或部分返回值.
+	uint256 _number;
+    bool _bool;
+    uint256[3] memory _array;
+    (_number, _bool, _array) = returnNamed();
+
+	// 读取部分返回值
+	(, _bool2, ) = returnNamed();
 }
 
 
