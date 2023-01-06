1714043f789cd7dc24e5cbd521d535c98fc54fef
lujetliu <462546997@qq.com>
2023-01-06 11:28:23 +0800

    inheritance

--- base/inheritance.sol
+++ base/inheritance.sol
@@ -77,10 +77,151 @@ pragma solidity ^0.8.4;
 			emit Log("Erzi");
 		}
 	}
+*/
 
 
+/*
+	修饰器的继承
+	Solidity中的修饰器(Modifier)同样可以继承, 用法与函数继承类似, 
+	在相应的地方加virtual和override关键字即可
+
+	contract Base1 {
+		modifier exactDividedBy2And3(uint _a) virtual {
+			require(_a % 2 == 0 && _a % 3 == 0);
+			_; // TODO:?
+		}
+	}
+
+	contract Identifier is Base1 {
+		//计算一个数分别被2除和被3除的值，但是传入的参数必须是2和3的倍数
+		function getExactDividedBy2And3(uint _dividend) public exactDividedBy2And3(_dividend) pure returns(uint, uint) {
+			return getExactDividedBy2And3WithoutModifier(_dividend);
+		}
+
+		//计算一个数分别被2除和被3除的值
+		function getExactDividedBy2And3WithoutModifier(uint _dividend) public pure returns(uint, uint){
+			uint div2 = _dividend / 2;
+			uint div3 = _dividend / 3;
+			return (div2, div3);
+		}
+	}
+
+	Identifier合约可以直接在代码中使用父合约中的exactDividedBy2And3修饰器,
+	也可以利用override关键字重写修饰器:
+	modifier exactDividedBy2And3(uint _a) override {
+        _;
+        require(_a % 2 == 0 && _a % 3 == 0);
+    }
+*/
+
+/*
+	构造函数的继承
+	子合约有两种方法继承父合约的构造函数, 以如下合约为例:
+
+	// 构造函数的继承
+	abstract contract A {
+		uint public a;
+
+		constructor(uint _a) {
+			a = _a;
+		}
+	}
+
+	- 在继承时声明父构造函数的参数, 例如: contract B is A(1)
+	- 在子合约的构造函数中声明构造函数的参数，例如:
+
+	contract C is A {
+		constructor(uint _c) A(_c * _c) {}
+	}
+*/
+
+/*
+	子合约调用父合约的函数
+	子合约有两种方式调用父合约的函数, 直接调用和利用super关键字;
+	1. 直接调用
+
+		子合约可以直接用父合约名.函数名()的方式来调用父合约函数, 例如Yeye.pop()
+		 function callParent() public{
+			Yeye.pop();
+		}
+
+	2. super 关键字
+		子合约可以利用super.函数名()来调用最近的父合约函数, solidity继承关系
+		按声明时从右到左的顺序是: contract Erzi is Yeye, Baba, 那么Baba是最
+		近的父合约, super.pop()将调用Baba.pop()而不是Yeye.pop()
+
+		function callParentSuper() public{
+			// 将调用最近的父合约函数，Baba.pop()
+			super.pop();
+		}
 
 */
 
-contract Inheritance{
+/*
+	钻石继承
+	在面向对象编程中, 钻石继承(菱形继承)指一个派生类同时有两个或两个以上的基类
+
+	在多重+菱形继承链条上使用super关键字时, 需要注意的是使用super会调用继承
+	链条上的每一个合约的相关函数, 而不是只调用最近的父合约;
+
+	写一个合约God, 再写Adam和Eve两个合约继承God合约, 最后创建合约people继承
+	自Adam和Eve, 每个合约都有foo和bar两个函数:
+
+*/
+
+/* 
+	继承树：
+	  God
+	 /  \
+	Adam Eve
+	 \  /
+	people
+*/
+
+contract God {
+    event Log(string message);
+
+    function foo() public virtual {
+        emit Log("God.foo called");
+    }
+
+    function bar() public virtual {
+        emit Log("God.bar called");
+    }
 }
+
+contract Adam is God {
+    function foo() public virtual override {
+        emit Log("Adam.foo called");
+		Adam.foo(); // TODO: 在函数的定义里调用函数?
+    }
+
+    function bar() public virtual override {
+        emit Log("Adam.bar called");
+        super.bar();
+    }
+}
+
+contract Eve is God {
+    function foo() public virtual override {
+        emit Log("Eve.foo called");
+        Eve.foo(); // TODO: 在函数的定义里调用函数?
+    }
+
+    function bar() public virtual override {
+        emit Log("Eve.bar called");
+        super.bar();
+    }
+}
+
+contract people is Adam, Eve {
+    function foo() public override(Adam, Eve) {
+        super.foo();
+    }
+
+    function bar() public override(Adam, Eve) {
+        super.bar();
+    }
+}
+
+
