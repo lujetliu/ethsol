d3585a043f11e0c387535084b582fc4a4b0bd00f
lujetliu <462546997@qq.com>
2023-02-16 15:05:23 +0800

    MultisignWallet

--- practice/MultisigWallet.sol
+++ practice/MultisigWallet.sol
@@ -1,7 +1,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;  
 
+/*
+	多签钱包
+	多签钱包是一种电子钱包, 特点是交易被多个私钥持有者(多签人)授权后才能执行:
+	例如钱包由3个多签人管理, 每笔交易需要至少2人签名授权; 多签钱包可以防止
+	单点故障(私钥丢失, 单人作恶), 更加去中心化, 更加安全, 被很多DAO采用
+
+
+	Gnosis Safe多签钱包是以太坊最流行的多签钱包, 管理近400亿美元资产, 
+	合约经过审计和实战测试, 支持多链(以太坊, BSC, Polygon等), 并提供丰
+	富的DAPP支持(TODO)
+	https://peopledao.mirror.xyz/nFCBXda8B5ZxQVqSbbDOn2frFDpTxNVtdqVBXGIjj0s
+
+	
+	多签钱包合约
+	在以太坊上的多签钱包其实是智能合约, 属于合约钱包; 本节实现一个极简版多签
+	钱包MultisigWallet合约, 由gnosis safe合约(几千行代码)简化而成
+	1. 设置多签人和门槛(链上): 部署多签合约时, 需要初始化多签人列表和执行
+		门槛(至少n个多签人签名授权后, 交易才能执行); Gnosis Safe多签钱包
+		还支持增加/删除多签人以及改变执行门槛, 但本节不实现
+
+	2. 创建交易(链下): 一笔待授权的交易包含以下内容
+		- to: 目标合约
+		- value: 交易发送的以太坊数量
+		- data: calldata, 包含调用函数的选择器和参数
+		- nonce: 初始为0, 随着多签合约每笔成功执行的交易递增的值, 可以防止
+			签名重放攻击
+	    - chainid: 链id, 防止不同链的签名重放攻击
+
+	3. 收集多签签名(链下): 将上一步的交易ABI编码并计算哈希, 得到交易哈希, 
+		然后让多签人签名, 并拼接到一起打包签名
+	
+	4. 调用多签合约的执行函数, 验证签名并执行交易(链上)
+	
+*/
+
 contract MultisigWallet {
+	// TODO: Remix 实验验证签名与教程不符
+	// (https://wtf.academy/solidity-application/MultisigWallet)
+	event ExecutionSuccess(bytes32 txHash);    // 交易成功事件
+    event ExecutionFailure(bytes32 txHash);    // 交易失败事件
+
+	address[] public owners;                   // 多签持有人数组
+    mapping(address => bool) public isOwner;   // 记录一个地址是否为多签
+    uint256 public ownerCount;                 // 多签持有人数量
+    uint256 public threshold;                  // 多签执行门槛, 交易至少有n个多签人签名才能被执行
+    uint256 public nonce;                      // nonce, 防止签名重放攻击 TODO
+
+    receive() external payable {}
+
 	// 构造函数, 初始化owners, isOwner, ownerCount, threshold 
 	constructor(        
 		address[] memory _owners,
